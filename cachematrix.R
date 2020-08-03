e5e1ab7b190de9a28a3b72960ebd7ff9576a8205
TheMaker <20593820+TheMaker42@users.noreply.github.com>
2020-08-03 01:51:31 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+#### A pair of functions for making matrices that can solve and internally cache their own values
 
-## Write a short comment describing this function
+
+## Create a matrix with built-in caching for inversions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	# Inner value containing the inverse matrix
+	m <- NULL
+	
+	# Changing the data (x) invalidates any existing inversion (m)
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() {
+		x
+	}
+	
+	# Inversion-related functions. These are only called by cacheSolve()
+	setInv <- function(y) {
+		m <<- y
+	}
+	getInv <- function() {
+		m
+	}
+	
+	list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## Return inverse of makeCacheMatrix from cache, performing calculation and storing result if missing.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	# Retrieve cached value from makeCacheMatrix(), or NULL if no value present.
+	m = x$getInv()
+	
+	if(is.null(m)) {
+		message("Performing inversion and updating cache")
+		m <- x$setInv(solve(x$get()))
+	}
+	else {
+		message("Returning cached inversion")
+		
+	}
+	
+	m
 }
